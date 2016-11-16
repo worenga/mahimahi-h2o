6cb2556777944bbe6ce4d0d5ac17d205c4356e80
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2016-11-16 21:53:52 +0100

    enable multiplexing

--- fcgi/FcgiHandler.py
+++ fcgi/FcgiHandler.py
@@ -7,4 +7,4 @@ from flup.server.fcgi import WSGIServer
 from ReplayApp import ReplayApp
 
 app = ReplayApp(os.environ['PUSH_STRATEGY_FILE'],os.environ['REPLAYSERVER_FN'])
-WSGIServer(app).run()
+WSGIServer(app,multiplexed=True).run()
