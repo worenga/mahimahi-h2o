c85913fa5db28f5f142a424afc39339b858bbe80
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2017-06-04 11:26:40 +0200

    push garbage in replay

--- fcgi/ReplayApp.py
+++ fcgi/ReplayApp.py
@@ -7,7 +7,8 @@ import subprocess
 from strip_chunked import decode
 from mimetools import Message
 from urlparse import urlparse
-
+import random
+import string
 
 class ReplayApp:
 
@@ -94,126 +95,136 @@ class ReplayApp:
         if env['HTTP_HOST'].endswith('.'):
            env['HTTP_HOST'] = env['HTTP_HOST'][:-1]
 
-        cached_response = None
-        is_push = False
-        if env['REQUEST_METHOD'] == "GET":
-            for u,host in enumerate(self.push_host):
-                if host == env['HTTP_HOST']:
-                  for v,push_resource in enumerate(self.push_assets[u]):
-                        parsed = urlparse(push_resource)
-                        stripped = push_resource[len('https://'+parsed.netloc):]
-                        push_resource = stripped
-                        if push_resource == env['REQUEST_URI']:
-                            #print "pushing from cache..."
-                            is_push = True
-                            cached_response = self.push_cache[u][v]
 
-        if cached_response is None:
-            # print environ['REQUEST_URI'], "Not cached!"
-            passed_env = dict()
+        #FastPath for Push Garbage:
+        if env['REQUEST_METHOD'] == "GET" and env['REQUEST_URI'].startswith('/__GARBAGE__'):
+            amount_kb = int(env['REQUEST_URI'].split('__')[2])
+            N = amount_kb * 1024
+            response = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+            start_response('200 OK', [('Content-Type', 'text/plain')])
+            yield response
+        else:
 
-            # remap for compat with replayserver
-            passed_env['MAHIMAHI_CHDIR'] = env['WORKING_DIR']
-            passed_env['MAHIMAHI_RECORD_PATH'] = env['RECORDING_DIR']
-            passed_env['REQUEST_METHOD'] = env['REQUEST_METHOD']
-            passed_env['REQUEST_URI'] = env['REQUEST_URI']
+            cached_response = None
+            is_push = False
+            if env['REQUEST_METHOD'] == "GET":
+                for u,host in enumerate(self.push_host):
+                    if host == env['HTTP_HOST']:
+                      for v,push_resource in enumerate(self.push_assets[u]):
+                            parsed = urlparse(push_resource)
+                            stripped = push_resource[len('https://'+parsed.netloc):]
+                            push_resource = stripped
+                            if push_resource == env['REQUEST_URI']:
+                                #print "pushing from cache..."
+                                is_push = True
+                                cached_response = self.push_cache[u][v]
 
-            # env['SERVER_PROTOCOL'], is currently a hack to find the corresponding
-            # h1 traces
-            passed_env['SERVER_PROTOCOL'] = "HTTP/1.1"
-            passed_env['HTTP_HOST'] = env['HTTP_HOST']
+            if cached_response is None:
+                # print environ['REQUEST_URI'], "Not cached!"
+                passed_env = dict()
 
-            #if 'HTTP_USER_AGENT' in env.keys():
-            #    passed_env['HTTP_USER_AGENT'] = env['HTTP_USER_AGENT']
+                # remap for compat with replayserver
+                passed_env['MAHIMAHI_CHDIR'] = env['WORKING_DIR']
+                passed_env['MAHIMAHI_RECORD_PATH'] = env['RECORDING_DIR']
+                passed_env['REQUEST_METHOD'] = env['REQUEST_METHOD']
+                passed_env['REQUEST_URI'] = env['REQUEST_URI']
 
-            if env['wsgi.url_scheme'] == 'https':
-                passed_env['HTTPS'] = "1"
+                # env['SERVER_PROTOCOL'], is currently a hack to find the corresponding
+                # h1 traces
+                passed_env['SERVER_PROTOCOL'] = "HTTP/1.1"
+                passed_env['HTTP_HOST'] = env['HTTP_HOST']
 
-            # shell=True,
-            p = subprocess.Popen(
-                [env['REPLAYSERVER_FN']], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=passed_env)
+                #if 'HTTP_USER_AGENT' in env.keys():
+                #    passed_env['HTTP_USER_AGENT'] = env['HTTP_USER_AGENT']
 
-            (cached_response, replay_stderr) = p.communicate()
+                if env['wsgi.url_scheme'] == 'https':
+                    passed_env['HTTPS'] = "1"
+
+                # shell=True,
+                p = subprocess.Popen(
+                    [env['REPLAYSERVER_FN']], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=passed_env)
+
+                (cached_response, replay_stderr) = p.communicate()
 
 
-        response_header, response_body = cached_response.split('\r\n\r\n', 1)
+            response_header, response_body = cached_response.split('\r\n\r\n', 1)
 
-        status_line, headers_alone = response_header.split('\r\n', 1)
-        splitted_status = status_line.split(' ')
+            status_line, headers_alone = response_header.split('\r\n', 1)
+            splitted_status = status_line.split(' ')
 
-        # response_code = status_line[1]
+            # response_code = status_line[1]
 
-        status_cleaned = ' '.join(splitted_status[1:])
+            status_cleaned = ' '.join(splitted_status[1:])
 
-        headers = Message(StringIO(headers_alone))
+            headers = Message(StringIO(headers_alone))
 
-        #print env['REQUEST_METHOD'], env['REQUEST_URI']
-       
-        is_chunked = False
-        corsfound = False
-        hdrlist = []
+            #print env['REQUEST_METHOD'], env['REQUEST_URI']
+           
+            is_chunked = False
+            corsfound = False
+            hdrlist = []
 
-        for key in headers.keys():
-            if key == "transfer-encoding" and 'chunked' in headers[key]:
-                is_chunked = True
-            else:
-                #Filter out any link header the site might emit.
-                if key not in ['expires', 'date', 'last-modified','link']:
-                    hdrlist.append((key.strip(), headers[key]))
-                if key.lower() == 'access-control-allow-origin':
-                    corsfound = True
-        
-        if not corsfound: 
-            # this is required for font hinting...
-            # note that we will not overwrite cors headers b/c
-            # in some xhr situations * is not sufficient
-            hdrlist.append(('Access-Control-Allow-Origin','*'))
+            for key in headers.keys():
+                if key == "transfer-encoding" and 'chunked' in headers[key]:
+                    is_chunked = True
+                else:
+                    #Filter out any link header the site might emit.
+                    if key not in ['expires', 'date', 'last-modified','link']:
+                        hdrlist.append((key.strip(), headers[key]))
+                    if key.lower() == 'access-control-allow-origin':
+                        corsfound = True
+            
+            if not corsfound: 
+                # this is required for font hinting...
+                # note that we will not overwrite cors headers b/c
+                # in some xhr situations * is not sufficient
+                hdrlist.append(('Access-Control-Allow-Origin','*'))
 
-        
-        if not is_push and env['SERVER_PROTOCOL'] == "HTTP/2":
-            for i, push_host_strategy in enumerate(self.push_host):
-                if passed_env['HTTP_HOST'] == push_host_strategy:
-                    if passed_env['REQUEST_URI'] == self.push_trigger_path[i]:
-                        linkstr = ''
-                        # TODO (bewo): is there any limitation?
-                        for asset in self.push_assets[i]:
-                            if linkstr != '':
-                                 linkstr += ','
-                            # print asset;
-                            linkstr += '<' + asset + '>; rel=preload'
-                        hdrlist.append(('x-extrapush', str(linkstr)))
-                        print 'WILL PUSH: ' ,len(self.push_assets[i]) #//, ('x-extrapush', str(linkstr))
-                        break
-
-        if not is_push:
-                for i, hint_host_strategy in enumerate(self.hint_host):
-                    if passed_env['HTTP_HOST'] == hint_host_strategy:
-                        if passed_env['REQUEST_URI'] == self.hint_trigger_path[i]:
+            
+            if not is_push and env['SERVER_PROTOCOL'] == "HTTP/2":
+                for i, push_host_strategy in enumerate(self.push_host):
+                    if passed_env['HTTP_HOST'] == push_host_strategy:
+                        if passed_env['REQUEST_URI'] == self.push_trigger_path[i]:
                             linkstr = ''
-                            for j, asset in enumerate(self.hint_assets[i]):
+                            # TODO (bewo): is there any limitation?
+                            for asset in self.push_assets[i]:
                                 if linkstr != '':
-                                   linkstr += ','
-                                as_string = self.hint_as_string[i][j]
-                                if as_string != '':
-                                   as_string='; as='+as_string+''
-                                if self.hint_as_string[i][j] == "font":
-                                   as_string += ";crossorigin"
-                                linkstr_to_append = '<'+asset + '>; rel=preload'+as_string+';type="'+self.hint_mimetype[i][j]+'"'
-                                linkstr += linkstr_to_append
-                            hdrlist.append(('link', str(linkstr)))
-                            print 'WILL HINT: ' ,len(self.hint_assets[i]) #//, ('x-extrapush', str(linkstr))
+                                     linkstr += ','
+                                # print asset;
+                                linkstr += '<' + asset + '>; rel=preload'
+                            hdrlist.append(('x-extrapush', str(linkstr)))
+                            print 'WILL PUSH: ' ,len(self.push_assets[i]) #//, ('x-extrapush', str(linkstr))
                             break
 
-        # print "start response! " + environ['REQUEST_URI']
+            if not is_push:
+                    for i, hint_host_strategy in enumerate(self.hint_host):
+                        if passed_env['HTTP_HOST'] == hint_host_strategy:
+                            if passed_env['REQUEST_URI'] == self.hint_trigger_path[i]:
+                                linkstr = ''
+                                for j, asset in enumerate(self.hint_assets[i]):
+                                    if linkstr != '':
+                                       linkstr += ','
+                                    as_string = self.hint_as_string[i][j]
+                                    if as_string != '':
+                                       as_string='; as='+as_string+''
+                                    if self.hint_as_string[i][j] == "font":
+                                       as_string += ";crossorigin"
+                                    linkstr_to_append = '<'+asset + '>; rel=preload'+as_string+';type="'+self.hint_mimetype[i][j]+'"'
+                                    linkstr += linkstr_to_append
+                                hdrlist.append(('link', str(linkstr)))
+                                print 'WILL HINT: ' ,len(self.hint_assets[i]) #//, ('x-extrapush', str(linkstr))
+                                break
 
-        if is_chunked:
-            # print "will decode chunked"
-            start_response(status_cleaned, hdrlist)
-            for chunk in decode(StringIO(response_body)):
-                yield str(chunk)
-        else:
-            start_response(status_cleaned, hdrlist)
-            yield response_body
+            # print "start response! " + environ['REQUEST_URI']
+
+            if is_chunked:
+                # print "will decode chunked"
+                start_response(status_cleaned, hdrlist)
+                for chunk in decode(StringIO(response_body)):
+                    yield str(chunk)
+            else:
+                start_response(status_cleaned, hdrlist)
+                yield response_body
 
         #start_response('200 OK', [('Content-Type', 'text/html')])
         # yield replay_stdout
