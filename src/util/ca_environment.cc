d71cfb7b1d6bc363048576b20cec1c0e4088bbe6
Benedikt Wolters <benedikt@cloudflare.com>
2018-12-01 14:29:22 +0100

    Make mahimahi-h2o compile with newer GCC version

--- src/util/ca_environment.cc
+++ src/util/ca_environment.cc
@@ -48,13 +48,13 @@ CAEnvironment::~CAEnvironment()
 {
     DIR *theFolder = opendir(this->dirname.c_str());
     struct dirent *next_file;
-    char filepath[256];
+    char filepath[1024];
 
     while ( (next_file = readdir(theFolder)) != NULL )
     {
         if (0==strcmp(next_file->d_name, ".") || 0==strcmp(next_file->d_name, "..")) { continue; }
         // build the path for each file in the folder
-        sprintf(filepath, "%s/%s", this->dirname.c_str(), next_file->d_name);
+        snprintf(filepath, sizeof(filepath), "%s/%s", this->dirname.c_str(), next_file->d_name);
         remove(filepath);
     }
     closedir(theFolder);
